d1e3ddc94d99e72ebb99e2c4d74654f7207076e7
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-07 11:21:24 -0300

    React_9_Listing Parte 2.2: Promise

--- listing.js
+++ listing.js
@@ -2,3 +2,17 @@ const fs = require("fs");
 fs.readdir("./", (error, paths) => {
   console.log(paths);
 });
+
+function readdir(path) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (error, paths) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(paths);
+      }
+    });
+  });
+}
+
+readdir("./").then((paths) => console.log(paths));
