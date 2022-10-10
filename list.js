c6588e41daaffc78a06671e3cbbff6fc997391a5
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-10 19:27:16 -0300

    React_9_Listing Parte 4.2: Stat na List

--- list.js
+++ list.js
@@ -15,3 +15,19 @@ function readdir(path) {
     });
   });
 }
+
+fs.stat("./", (error, paths) => {
+  console.log(paths);
+});
+
+function stat(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (error, paths) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(paths);
+      }
+    });
+  });
+}
