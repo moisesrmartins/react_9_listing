6b0aa6a29a07a433aa10813891f4148d7d6e7146
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-10 18:28:43 -0300

    React_9_Listing Parte 3.2: Promise

--- stat.js
+++ stat.js
@@ -3,3 +3,17 @@ const fs = require("fs");
 fs.stat("./", (error, paths) => {
   console.log(paths);
 });
+
+function stat(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (error, paths) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(paths);
+      }
+    });
+  });
+}
+
+stat("./").then((paths) => console.log(paths));
